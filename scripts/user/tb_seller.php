d956aa999d712991e26f0d955ffb1d9f06d7bb48
davidruc <davidarueda18@gmail.com>
2023-06-07 16:05:35 -0500

    feat: :sparkles: creación de clases
    no estoy seguro de si esta creación de métodos de variables sea el mejor

--- scripts/user/tb_seller.php
+++ scripts/user/tb_seller.php
@@ -1,5 +1,9 @@
 <?php 
 
 class tb_seller extends tb_user{
-    
+    public function __construct(public $NameSeller){}
+    use getInstance;
+    public function getNameSeller(){
+        return $this->NameSeller;
+    }
 }
--- scripts/user/tb_user.php
+++ scripts/user/tb_user.php
@@ -2,18 +2,14 @@
 // TODO: Metodología singelton -> La variable se instancia sola
 
 class tb_user{
-    public $NroBill;
-    public $BillDate;
-    public $fullName;
-    public function __construct($NroBill, $BillDate, $fullName){
-        $this->NroBill = $NroBill;
-        $this->BillDate = $BillDate;
-        $this->fullName = $fullName;
+    public function __construct(public $NroBill,public $BillDate,public $fullName){}
+    use getInstance;
+    public function getNroBill(){
+        return $this->NroBill;
+    }
+    public function getBillDate(){
+        return $this->BillDate;
     }
 }
-
-
-    
-
 
 ?>
